13c45063af5c1c159db5522267ea507d441eb9bd
Mehdi.Zareiane <affrosamuraii@gmail.com>
2025-01-12 20:20:06 +0330

    example

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-const firstNumber = +prompt("enter your number.");
-const secondNumber = +prompt("emter your second number.");
-const operator = prompt("enter sign");
-
-if (isNaN(firstNumber + secondNumber)) {
-  console.log("sth Wrong!");
-} else {
-  switch (operator) {
-    case "+":
-      console.log(firstNumber + secondNumber);
-      break;
-    case "-":
-      console.log(firstNumber - secondNumber);
-      break;
-    case "*":
-      console.log(firstNumber * secondNumber);
-      break;
-    case "/":
-      console.log(firstNumber / secondNumber);
-      break;
-      default : console.log("sth is wrong2.")
-  }
+function sum(stuff = "noon"){
+    return stuff
 }
+console.log(sum("panir"));
+console.log(sum("moz"));
+console.log(sum());
